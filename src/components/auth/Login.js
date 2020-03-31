d05d65a31a071244e53f886938f0778dffaa62cd
Alejandro Toro <alejandro@kommit.co>
2020-03-31 09:15:00 -0500

    Add the reusable inputs to the login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import FormInput from '../shared/formInput';
+import { Button } from '../shared/button';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+  const onChange = () => {};
+
   return (
-    <div>
-      Login
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <form>
+          <h3>Sign In</h3>
+          <div className="form-group">
+            <FormInput
+              type="text"
+              name="username"
+              label="Username"
+              className="form-control"
+              placeholder="Enter Username"
+              value=""
+              error="You need to enter a valid username"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <FormInput
+              type="password"
+              name="password"
+              label="Password"
+              className="form-control"
+              placeholder="Enter Password"
+              value=""
+              error="You need to enter a valid password"
+              onChange={onChange}
+            />
+          </div>
+          <Button
+            type="submit"
+            label="Sign Up"
+            className="btn btn-primary btn-block"
+          />
+          <p className="forgot-password text-right">
+            Not yet registered? <Link to={"/sign-up"}>Sign Up</Link>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
